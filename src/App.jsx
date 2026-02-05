e7403ef77e56c6c29bed61fea408c8a761afa027
Ahmed Abdirizaak <mernstack.js@gmail.com>
2026-02-05 01:40:05 +0100

    feat: update routing and authentication flow in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import { Link, Route, Routes } from "react-router";
+import { Link, Navigate, Route, Routes } from "react-router";
 import Todos from "./pages/Todos";
 import AddTodos from "./pages/Add-Todos";
+import Home from "./pages/Home";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Login from "./pages/login";
+import { UseTodos } from "./util/todosContext";
 
 export default function App() {
+  const { user } = UseTodos();
+
   return (
     <div className="p-12">
-      <header className="flex gap-3 border-b cursor-pointer mb-8">
-        <Link to="/">Todos</Link>
-        <Link to="/add-todos">Add Todos</Link>
-      </header>
       <Routes>
-        <Route path="/" element={<Todos />} />
-        <Route path="/add-todos" element={<AddTodos />} />
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/login"
+          element={!user?.isLoggedIn ? <Login /> : <Navigate to="/todos" />}
+        />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/todos" element={<Todos />} />
+          <Route path="/add-todos" element={<AddTodos />} />
+        </Route>
       </Routes>
     </div>
   );
